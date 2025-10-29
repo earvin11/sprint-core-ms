85b35069f52960c774ef4a186ea870f43b2d1a2c
Earvin Gomez <earvinsantiago.11@gmail.com>
2025-10-29 07:47:40 -0400

    fields bets in roulette model

--- src/games/domain/implementations/roulette.value.ts
+++ src/games/domain/implementations/roulette.value.ts
@@ -21,6 +21,18 @@ export class Roulette implements RouletteEntity {
   public urlTransmision: string;
   public uuid?: string;
 
+  public calle?: number;
+  public chanceSimple?: number;
+  public columna?: number;
+  public cuadro?: number;
+  public cubre?: number;
+  public docena?: number;
+  public linea?: number;
+  public pleno?: number;
+  public saveRecordings?: boolean;
+  public semipleno?: number;
+  public specialCalle?: number;
+
   constructor(data: RouletteEntity) {
     this.alertEmails = data.alertEmails;
     this.initialBank = data.initialBank;
@@ -39,5 +51,17 @@ export class Roulette implements RouletteEntity {
     this.providerId = data.providerId;
     this.urlTransmision = data.urlTransmision;
     this.uuid = randomUUID();
+    this.saveRecordings = data.saveRecordings;
+
+    this.calle = data.calle;
+    this.chanceSimple = data.chanceSimple;
+    this.columna = data.columna;
+    this.cuadro = data.cuadro;
+    this.cubre = data.cubre;
+    this.docena = data.docena;
+    this.linea = data.linea;
+    this.pleno = data.pleno;
+    this.semipleno = data.semipleno;
+    this.specialCalle = data.specialCalle;
   }
 }
