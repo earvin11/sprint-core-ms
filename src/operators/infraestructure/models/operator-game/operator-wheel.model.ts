c6f42d94714f170a49f3c0bb4d4b7d20d232a7a1
Earvin Gomez <earvinsantiago.11@gmail.com>
2025-10-31 11:08:31 -0400

    advanced in operatorGame

---
+++ src/operators/infraestructure/models/operator-game/operator-wheel.model.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { OperatorGame } from './operator-game.model';
+import { ConfigPayment, OperatorWheelEntity } from 'src/operators/domain/entities/operator-game/operator-wheel.entity';
+
+@Schema()
+export class OperatorWheel
+  extends OperatorGame
+  implements OperatorWheelEntity
+{
+    @Prop()
+    configPayment: ConfigPayment[];
+}
+
+export const OperatorWheelSchema =
+  SchemaFactory.createForClass(OperatorWheel);
