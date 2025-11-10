d8f9e7e31615788522dee7076c151cc243592cca
Earvin <earvinsantiago.11@gmail.com>
2025-11-10 10:29:15 -0400

    lobby init

--- src/launch/application/lobby.use-case.ts
+++ src/launch/application/lobby.use-case.ts
@@ -5,7 +5,15 @@ import { OperatorGameUseCases } from '../../operators/application/operator-game/
 import { OperatorLimitsUseCases } from 'src/operators/application/operator-limits/operator-limits.use-cases';
 import { OperatorChipUseCases } from 'src/operators/application/operator-chip.use-cases';
 import { OperatorCurrencyUseCases } from 'src/operators/application/operator-currency.use-cases';
+import { OperatorGameEntity } from 'src/operators/domain/entities/operator-game/operator-game.entity';
 
+export interface LobbyRequestInterface {
+  token: string;
+  operatorId: string;
+  language: string;
+  casinoToken: string;
+  currency: string;
+}
 @Injectable()
 export class LobbyUseCase {
   constructor(
@@ -17,5 +25,33 @@ export class LobbyUseCase {
     private readonly operatorLimitsUseCases: OperatorLimitsUseCases,
   ) {}
 
-  async run() {}
+  async run(input: LobbyRequestInterface) {
+    const { operatorId, casinoToken, currency } = input;
+
+    const operator = await this.operatorUseCases.findById(operatorId);
+    if (!operator) return { error: true, message: 'Operator not found' };
+    if (!operator.status || !operator.available)
+      return { error: true, message: 'Operator block' };
+
+    const client = await this.clientUseCases.findById(operator.client);
+    if (!client) return { error: true, message: 'Client not found' };
+    if (!client.status || !client.status)
+      return { error: true, message: 'Client block or disabled' };
+    if (client.token !== casinoToken)
+      return { error: true, message: 'Casino token invalid' };
+
+    const gamesInOperator = await this.operatorGameUseCases.findManyBy({
+      operator,
+    });
+    //TODO:
+    const games = gamesInOperator.map((operatorGame: OperatorGameEntity) => {
+      if (operatorGame.currencies.includes(currency)) {
+        return operatorGame;
+      }
+    });
+
+    return {
+      games,
+    };
+  }
 }
